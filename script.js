ea81e99abd33b3c85f50f76fc49fad7ac08faf07
supakit0607 <tonerory@gmail.com>
2025-08-13 11:02:26 +0700

    test

---
+++ script.js
@@ -0,0 +1,48 @@
+const apiKey = '703724cb4c6a5168dfe9c96a8e2e201a';
+
+const searchForm = document.querySelector('#search-form');
+const cityInput = document.querySelector('#city-input');
+const weatherInfoContainer = document.querySelector('#weather-info-container');
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const cityName = cityInput.value.trim();
+
+    if (cityName) {
+        getWeather(cityName);
+    } else {
+        alert('กรุณาป้อนชื่อเมือง');
+    }
+});
+
+async function getWeather(city) {
+    weatherInfoContainer.innerHTML = `<p>กำลังโหลดข้อมูล...</p>`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=th`;
+ //https://api.openweathermap.org/data/2.5/weather?q=bangkok&appid=703724cb4c6a5168dfe9c96a8e2e201a&units=metric&lang=th
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('ไม่พบข้อมูลเมืองนี้');
+        }
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        weatherInfoContainer.innerHTML = `<p class="error">${error.message}</p>`;
+    }
+}
+
+function displayWeather(data) {
+    const { name, main, weather } = data;
+    const { temp, humidity } = main;
+    const { description, icon } = weather[0];
+
+    const weatherHtml = `
+        <h2>${name}</h2>
+        <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}">
+        <p style="font-size: 2rem; font-weight: bold;">${temp.toFixed(1)}°C</p>
+        <p>${description}</p>
+        <p>ความชื้น: ${humidity}%</p>
+    `;
+    weatherInfoContainer.innerHTML = weatherHtml;
+}
